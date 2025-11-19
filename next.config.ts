e33b7bee60ccf94ad2e477f183b6faf14b450b5f
Agustin Molina <am.motos@hotmail.com>
2025-11-19 16:46:35 -0300

    puppetter cire

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  serverExternalPackages: [
+    "puppeteer-core",
+    "puppeteer-extra",
+    "puppeteer-extra-plugin-stealth",
+    "@sparticuz/chromium-min",
+  ],
 };
 
 export default nextConfig;
